e2c6e18c952e57fe5a389d2ce519bc08d9061605
Animesh-DS <animeshsingh519@gmail.com>
2026-03-18 20:30:23 +0530

    -

---
+++ Codes/Image_dim.py
@@ -0,0 +1,14 @@
+import cv2
+
+# read image
+img = cv2.imread("Image/image.png")
+
+print("Shape:", img.shape)
+print("Height:", img.shape[0])
+print("Width:", img.shape[1])
+print("Channels:", img.shape[2])
+
+#channel - represent how many values are used per pixel to show any color of an image
+
+# print(img)
+# print(type(img))
